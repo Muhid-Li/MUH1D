dbf9dfccd9b414a1dd4069711a6addc4146d1905
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-03-10 18:56:32 +0600

    Update RANDOM.py

--- RANDOM.py
+++ RANDOM.py
@@ -1,6 +1,9 @@
-import os
+import os,platform
+os.system('clear')
+print('\033[1;34m[•] Checking Updates...')
+os.system('git pull')
+print('\033[1;32m[•] Congrats! Your Device Support This Tools')
 
-print("update cheking")
 
 os.system("git pull")
 
